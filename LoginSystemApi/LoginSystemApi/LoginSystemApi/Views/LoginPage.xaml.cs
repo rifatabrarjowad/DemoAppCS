b2a1fef5b6ef8ac922ea23a6273598a2d615aa34
rifatabrarjowad <rajdiprifat2004@gmail.com>
2021-12-25 15:54:18 +0600

    update

---
+++ LoginSystemApi/LoginSystemApi/LoginSystemApi/Views/LoginPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using LoginSystemApi.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LoginSystemApi.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+            this.BindingContext = new LoginViewModel();
+        }
+        async void OnButtonClicked(object sender, EventArgs args)
+        {
+            //await label.RelRotateTo(360, 1000);
+            string usrName = usr.Text;
+            string usrPass = passs.Text;
+            if (usrName != null || usrPass != null)
+            {
+                if (usrName == usrPass)
+                {
+                    await Shell.Current.GoToAsync("//AboutPage");
+                }
+                else
+                {
+                    label.Text = "faild";
+                    await label.RelRotateTo(360, 1000);
+                }
+            }
+            else
+            {
+                alt.Text = "pls give us your information ";
+                alt.BackgroundColor = Color.Red;
+                alt.IsVisible = true;
+            }
+           
+        }
+        async void OnButtonSingUp(object sender, EventArgs args)
+        {
+            sing.IsVisible = false;
+            label.Text = "Sing In ";
+            log.IsVisible = true;
+            singbtn.IsVisible = true;
+            logbtn.IsVisible = false;
+        }
+        async void OnButtonLogin(object sender, EventArgs args)
+        {
+            sing.IsVisible = false;
+            label.Text = "Log IN ";
+            log.IsVisible = true;
+            singbtn.IsVisible = false;
+            logbtn.IsVisible = true;
+            
+        }
+    }
+}
--- SqlLite/SqlLite/SqlLite.Android/obj/Debug/120/android/environment.x86_64.s
+++ SqlLite/SqlLite/SqlLite.Android/obj/Debug/120/android/environment.x86_64.s
@@ -78,7 +78,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"791821ee-b74f-4c9b-b977-40dc0a669d67"
+	.asciz	"b32d79f1-430b-4ace-a760-5281534264ce"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
